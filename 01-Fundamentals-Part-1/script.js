f6f7a6f40db762126d6f705948f0e9435450ded6
familliarous <geoffabis@gmail.com>
2025-03-11 16:03:12 +0400

    Tested js conventions

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,18 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is fun!');
 console.log(40 + 9 + 23 - 10);
+
+let firstName = 'Jonas';
+
+let lastName = 'Smith';
+
+let PI = 3.1415;
+
+let $dollar = 40;
+
+console.log(js);
+console.log(firstName);
+console.log(lastName);
+console.log(PI);
+console.log($dollar);
+
+console.log(firstName);
--- JS Course Notes/Section 2 - Javascript Fundamentals Pt 1/Part 8 - Linking a Javascript File.md
+++
@@ -1,5 +0,0 @@
-
-# [11/03/2025] Notes
-
-- The developer console in modern browsers are just an environment to mostly execute pieces of code.
-- Website Content should be separate from the JS logic. Inline JS is only good for short lines of code
