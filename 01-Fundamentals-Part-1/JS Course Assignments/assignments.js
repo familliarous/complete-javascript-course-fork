87510333a2907ff10d87d5269cb85ee564f32fa7
familliarous <geoffabis@gmail.com>
2025-03-11 16:12:05 +0400

    Completed first assignment

---
+++ 01-Fundamentals-Part-1/JS Course Assignments/assignments.js
@@ -0,0 +1,9 @@
+// VALUES AND VARIABLES
+
+let country = 'Philippines';
+let continent = 'Asia';
+let population = '114,900,000';
+
+console.log(country);
+console.log(continent);
+console.log(population);
