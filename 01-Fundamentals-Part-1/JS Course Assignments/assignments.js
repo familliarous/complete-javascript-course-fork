bff7fa162c72534b5f6f0b6dd2094118129433db
familliarous <geoffabis@gmail.com>
2025-03-11 16:13:43 +0400

    Completed Data types assignment

--- 01-Fundamentals-Part-1/JS Course Assignments/assignments.js
+++ 01-Fundamentals-Part-1/JS Course Assignments/assignments.js
@@ -7,3 +7,12 @@ let population = '114,900,000';
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// DATA TYPES
+
+let isIsland = true;
+var language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof language);
+console.log(typeof country);
