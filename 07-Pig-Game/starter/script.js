61d1fc0d6f3fae0b8dc5a6e7cddfcfbb66f03c8f
familliarous <geoffabis@gmail.com>
2025-04-02 17:00:10 +0400

    Implemented dice score display in pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,6 +3,8 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const buttonNew = document.querySelector('.btn--new');
 const buttonRoll = document.querySelector('.btn--roll');
@@ -11,6 +13,7 @@ const buttonHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+let currentScore = 0;
 
 buttonRoll.addEventListener('click', function () {
   // 1. Generate random dice roll
@@ -20,4 +23,11 @@ buttonRoll.addEventListener('click', function () {
   diceEl.src = `dice-${dice}.png`;
 
   // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice value to the current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // Switch to the next player
+  }
 });
