84b58bcefcd889e40d45ffd84d0d3c72e691879a
familliarous <geoffabis@gmail.com>
2025-04-02 16:52:46 +0400

    Implemented dice rolling in pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -4,7 +4,20 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const buttonNew = document.querySelector('.btn--new');
+const buttonRoll = document.querySelector('.btn--roll');
+const buttonHold = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+buttonRoll.addEventListener('click', function () {
+  // 1. Generate random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. Display Dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, switch to next player
+});
