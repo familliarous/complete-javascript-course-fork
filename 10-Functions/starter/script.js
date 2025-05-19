cfff5b83e0004a203123ec19edec6edde17fe267
familliarous <geoffabis@gmail.com>
2025-05-19 16:45:50 +0400

    136. How passing Arguments work

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers,
+) {
+  // use short circuiting (ES5):
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+
+createBooking('LH123', 2, 800);
+
+createBooking('LH123', 2);
+
+createBooking('LH123', 5);
+
+// can't skip parameters:
+
+createBooking('LH123', 2999);
+
+// put undefined so you can "skip"
+
+createBooking('LH123', undefined, 1000);
--- JS Course Notes/Section 10 - A Closer Look at Functions/Section 10 - A closer Look at Functions.md
+++ JS Course Notes/Section 10 - A Closer Look at Functions/Section 10 - A closer Look at Functions.md
@@ -3,6 +3,32 @@
 
 # 133. Section Intro
 
-# 134. Seciton Roadmap
+- More on functions
+# 134. Section Roadmap
 
+- ![[Pasted image 20250519164001.png]]
 # 135. Default Parameters
+
+You can use enhanced object literal syntax for passing function parameters to an object.
+
+```
+// Syntax:
+const createBooking = function (flightNum, numPassengers, price) {
+
+  const booking = {
+
+    flightNum,
+
+    numPassengers,
+
+    price,
+
+  };
+
+  console.log(booking);
+
+  bookings.push(booking);
+
+};
+```
+
