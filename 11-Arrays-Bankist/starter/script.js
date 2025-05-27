4a40dbca6aa16c0d28634739590998d3a42492cf
familliarous <geoffabis@gmail.com>
2025-05-27 16:16:31 +0400

    finished  149. Simple Array Methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,52 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+
+console.log([...arr]);
+
+// SPLICE
+
+// console.log(arr.splice(2)); // modifies the original array
+arr.splice(-1); // also modifies the original array
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE
+
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse()); // modifies original array
+console.log(arr2);
+
+// CONCAT
+
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log(...arr, ...arr2); // spread operator
+
+// JOIN
+
+console.log(letters.join(' - '));
+
+// 150. The new at method
+console.log('// 150. The new at method');
+
+const arrNew = [23, 11, 64];
+
+console.log(arr[0]);
+console.log(arr.at(0)); // the same as indexing [0].
+
+// why use this if it's just similar?
+// use cases: getting the last element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-2));
--- JS Course Notes/Section 11 - Working with Arrays (PROJECT)/Section 11 - Working with Arrays (PROJECT).md
+++ JS Course Notes/Section 11 - Working with Arrays (PROJECT)/Section 11 - Working with Arrays (PROJECT).md
@@ -3,4 +3,13 @@
 
 # 149. Simple Array Methods
 
+- splice() - modifies the array, and removes existing elements based on the parameter given (index)
+- reverse() - modifies the array by reversing its contents' order. 
+- concat() - joins the contents of two arrays together.
+- join() - can also be done with the spread operator (...). Joins the values of an array into one String, separated by whatever character you put in the parameter.
+
+# [27/05/2025] NOTES
+
+# 150. The New at Method
+
 - 
