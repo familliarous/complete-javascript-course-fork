941fbb275ee175b6e4ddaee1b79b0e972e31b594
familliarous <geoffabis@gmail.com>
2025-05-27 16:45:42 +0400

    finished  151. Looping Arrays: forEach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -126,3 +126,36 @@ console.log(arr.at(-2));
 
 // you can use this for strings also:
 console.log('jonas'.at(0));
+
+// 151. Looping arrays: forEach
+
+console.log('151. Looping arrays: forEach');
+
+const movements2 = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const movement of movements2) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('-----FOREACH-----');
+// forEach method requires a callback function to tell it what to do per iteration
+movements.forEach(function (mov, i, arr) {
+  // 1st param: current element, 2nd param: current index, 3rd param: entire array being looped over
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+
+// When to use foreach method and for of:
+// CANNOT break outside of a for each loop.
+// You need a for-of loop for that.
--- JS Course Notes/Section 11 - Working with Arrays (PROJECT)/Section 11 - Working with Arrays (PROJECT).md
+++ JS Course Notes/Section 11 - Working with Arrays (PROJECT)/Section 11 - Working with Arrays (PROJECT).md
@@ -12,4 +12,35 @@
 
 # 150. The New at Method
 
-- 
+- Syntax: 
+```
+const arrNew = [23, 11, 64];
+
+  
+
+console.log(arr[0]);
+
+console.log(arr.at(0)); // the same as indexing [0].
+
+  
+
+// why use this if it's just similar?
+
+// use cases: getting the last element
+
+console.log(arr[arr.length - 1]);
+
+console.log(arr.slice(-1)[0]);
+
+console.log(arr.at(-2));
+
+  
+
+// you can use this for strings also:
+
+console.log('jonas'.at(0));
+```
+
+# 151. Looping Arrays: forEach
+
+- Syntax:
